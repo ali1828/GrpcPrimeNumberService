1cf2510ca8d0598864115c12a6b340fc28c9d7a0
AliAshraf <aashraf@ncspvt.com>
2023-06-25 23:57:20 +0500

    Implementation for the proto procedure IsPrimeStream. 1. Added TotalMessagesReceived long variable for tracking number of messages received. Locking is applied for thread safety. ConcurrentDictioanry used for all valid prime numbers. Code refactor ( Make a new method for NumberIsPrime for code readability, scalibility and resuability. Handling of any garbage call from client like <=1.

--- GrpcPrimeNumberService/Services/PrimeNumberService.cs
+++ GrpcPrimeNumberService/Services/PrimeNumberService.cs
@@ -1,14 +1,79 @@
 ﻿using Grpc.Core;
 using GrpcService1;
+using System.Collections.Concurrent;
 
 namespace GrpcPrimeNumberService.Services
 {
     public class PrimeNumberService : Primenumber.PrimenumberBase
     {
+        private static object lockObject = new object();
+        private static long TotalMessagesReceived = 0;
         private readonly ILogger<PrimeNumberService> _logger;
         public PrimeNumberService(ILogger<PrimeNumberService> logger)
         {
             _logger = logger;
+        }
+
+        ConcurrentDictionary<long, long> primeNumberDictionary = new ConcurrentDictionary<long, long>();
+
+        public override async Task IsPrimeStream(IAsyncStreamReader<RequestPrimeNumber> requestStream,
+            IServerStreamWriter<ReplyPrimeNumber> responseStream, ServerCallContext context)
+        {
+            var response = new ReplyPrimeNumber();
+            await foreach (RequestPrimeNumber request in requestStream.ReadAllAsync())
+            {
+                lock (lockObject)
+                {
+                    TotalMessagesReceived++;
+                }
+
+                bool IsPrime = true;
+                Int64 requestedNumber = request.Number;
+
+                if (requestedNumber <= 1)
+                {
+                    response.Result.Add(new RequestPrimeNumber
+                    {
+                        Id = request.Id,
+                        Number = requestedNumber,
+                        Message = requestedNumber + " not prime.",
+                        Timestamp = request.Timestamp
+                    });
+                }
+                else
+                {
+                    NumberIsPrime(response, request, IsPrime, requestedNumber);
+                }
+
+            }
+
+            await responseStream.WriteAsync(response);
+
+        }
+
+        private void NumberIsPrime(ReplyPrimeNumber response, RequestPrimeNumber request, bool IsPrime, long requestedNumber)
+        {
+            Int64 iterations = requestedNumber / 2;
+            for (int i = 2; i <= iterations; i++)
+            {
+                if (requestedNumber % i == 0)
+                {
+                    IsPrime = false;
+
+                    response.Result.Add(new RequestPrimeNumber
+                    { Id = request.Id, Number = requestedNumber, Message = requestedNumber + " not prime.", Timestamp = request.Timestamp });
+                    break;
+                }
+            }
+            if (IsPrime)
+            {
+                if (primeNumberDictionary.Where(p => p.Key == request.Number).Count() == 0)
+                {
+                    primeNumberDictionary.TryAdd(requestedNumber, requestedNumber);
+                }
+                response.Result.Add(new RequestPrimeNumber
+                { Id = request.Id, Number = requestedNumber, Message = requestedNumber + " IS PRIME NUMBER.", Timestamp = request.Timestamp });
+            }
         }
     }
 }
