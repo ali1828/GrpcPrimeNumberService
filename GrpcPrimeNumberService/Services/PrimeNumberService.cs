31e2c3849ab67ef304575f114a69a6ac05642855
AliAshraf <aashraf@ncspvt.com>
2023-06-25 23:47:31 +0500

    Added primenumber.proto to csproj for referencing and auto generating internal proto files by .Net, Inheriting Primenumber.PrinumberBase generated by .Net

--- GrpcPrimeNumberService/Services/PrimeNumberService.cs
+++ GrpcPrimeNumberService/Services/PrimeNumberService.cs
@@ -1,6 +1,14 @@
-﻿namespace GrpcPrimeNumberService.Services
+﻿using Grpc.Core;
+using GrpcService1;
+
+namespace GrpcPrimeNumberService.Services
 {
-    public class PrimeNumberService
+    public class PrimeNumberService : Primenumber.PrimenumberBase
     {
+        private readonly ILogger<PrimeNumberService> _logger;
+        public PrimeNumberService(ILogger<PrimeNumberService> logger)
+        {
+            _logger = logger;
+        }
     }
 }
